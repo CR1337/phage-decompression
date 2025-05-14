a8fd6bdd1fabf54f5d0aff15ee4e466b6433e425
Christian Raue <captain_2@gmx.de>
2025-05-14 14:35:39 +0200

    analysis

---
+++ analyze.py
@@ -0,0 +1,95 @@
+import os
+import statistics
+from tqdm import tqdm
+from dataclasses import dataclass
+from typing import List
+from numbers import Number
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from biotite.sequence.io import fasta
+from biotite.sequence import NucleotideSequence
+
+
+PROCESSED_NAMES_FILENAME: str = "processed_names.txt"
+GENOME_DIRECTORY: str = "GenomesDB"
+
+
+@dataclass
+class GenomeAnalysis:
+    original_length: int
+    decompressed_length: int
+
+    @property
+    def compression_ratio(self) -> float:
+        return self.decompressed_length / self.original_length
+    
+    @property
+    def difference(self) -> int:
+        return self.decompressed_length - self.original_length
+    
+
+def analyze(name: str) -> GenomeAnalysis:
+    fasta_filename = os.path.join(GENOME_DIRECTORY, f"{name}.fasta")
+    decompressed_fasta_filename = os.path.join(GENOME_DIRECTORY, name, f"{name}_decompressed.fasta")
+
+    fasta_file = fasta.FastaFile.read(fasta_filename)
+    decompressed_fasta_file = fasta.FastaFile.read(decompressed_fasta_filename)
+
+    genome = fasta.get_sequence(fasta_file, seq_type=NucleotideSequence)
+    decompressed_genome = fasta.get_sequence(decompressed_fasta_file, seq_type=NucleotideSequence)
+
+    return GenomeAnalysis(len(genome), len(decompressed_genome))
+
+
+def describe(data: List[Number], title: str):
+    print(title)
+    print("-" * len(title))
+    print(f"Mean: {statistics.mean(data)}")
+    print(f"Median: {statistics.median(data)}")
+    print(f"StdDev: {statistics.stdev(data)}")
+    print(f"Min: {min(data)}")
+    print(f"Max: {max(data)}")
+    print()
+
+
+def plot(data: List[Number], title: str, ax):
+    sns.kdeplot(data, fill=True, color="skyblue", label="KDE", ax=ax)
+    ax.set_title(title)
+    ax.set_xlabel("Value")
+    ax.set_ylabel("Density")
+
+
+def main():
+    with open(PROCESSED_NAMES_FILENAME, 'r') as file:
+        names = (n.strip() for n in file.readlines())
+        names = [n for n in names if len(n) > 0]
+
+    analyses = [
+        analyze(name) for name 
+        in tqdm(names, total=len(names), desc="Analyzing genomes")
+    ]
+
+    original_lengths = [a.original_length for a in analyses]
+    decompressed_lengths = [a.decompressed_length for a in analyses]
+    compression_ratios = [a.compression_ratio for a in analyses]
+    differences = [a.difference for a in analyses]
+
+    fig, axs = plt.subplots(2, 2, figsize=(12, 12))
+    axs_flat = axs.ravel()
+    for data, title, ax in zip(
+        [original_lengths, decompressed_lengths, compression_ratios, differences],
+        ["Original lenghts", "Decompressed lengths", "Compression ratios", "Differences"],
+        axs_flat
+    ):
+        describe(data, title)
+        plot(data, title, ax)
+
+    fig.tight_layout()
+    filename = os.path.join("plots", "analysis.png")
+    fig.savefig(filename)
+
+
+if __name__ == "__main__":
+    main()
