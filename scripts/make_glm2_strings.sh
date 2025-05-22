5e6ede01847f9ed79b6146892d6bcdf85ec13ed7
Christian Raue <captain_2@gmx.de>
2025-05-22 10:19:34 +0200

    adapted path

--- scripts/make_glm2_strings.sh
+++ scripts/make_glm2_strings.sh
@@ -3,4 +3,4 @@
 INPUT_DIR="$1"
 NUM_FILES=$(ls "$INPUT_DIR"/*.fasta | wc -l)
 
-sbatch --export=DIR="$INPUT_DIR",ENV_PATH=".Svenv" --array=0-$(($NUM_FILES - 1)) _make_glm2_strings_array.sh
+sbatch --export=DIR="$INPUT_DIR",ENV_PATH=".Svenv" --array=0-$(($NUM_FILES - 1)) scripts/_make_glm2_strings_array.sh
